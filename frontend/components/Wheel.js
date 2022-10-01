f8a84c30b8dcd4b2abfdfdeaaf97aa743a37fcdd
Jordon Pruitt <102494668+JordonP123@users.noreply.github.com>
2022-09-30 21:38:50 -0400

    Update Wheel.js

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -20,7 +20,7 @@ function Wheel(props) {
         <div className={props.wheel === 2 ? "cog active" : "cog"} style={{ "--i": 2 }}>{props.wheel === 2 ? 'B' : ''}</div>
         <div className={props.wheel === 3 ? "cog active" : "cog"} style={{ "--i": 3 }}>{props.wheel === 3 ? 'B' : ''}</div>
         <div className={props.wheel === 4 ? "cog active" : "cog"} style={{ "--i": 4 }}>{props.wheel === 4 ? 'B' : ''}</div>
-        <div className={props.wheel === 5 ? "cog active" : "cog"} style={{ "--i": 5 }}>{props.wheel === 5 ? 'B' : ''}</div>{/* --i is a custom CSS property, no need to touch that nor the style object */}
+        <div className={props.wheel === 5 ? "cog active" : "cog"} style={{ "--i": 5 }}>{props.wheel === 5 ? 'B' : ''}</div>
       </div>
       <div id="keypad">
         <button onClick={moveCounterClockwise} id="counterClockwiseBtn" >Counter clockwise</button>
